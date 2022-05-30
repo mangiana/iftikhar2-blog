d0d0e4246e719245e320ac6a3e39cfc427fb92a8
iftikhar <iftikharmangiana.official@gmail.com>
2022-05-30 23:48:19 +0500

    Add create action in User Controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,23 @@
 class UsersController < ApplicationController
 
-    def new
-      @user = User.new
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+        flash[:notice] = "Welcome To Iftikhar Blog! #{@user.username} You have Successfully Signed Up"
+        redirect_to articles_path
+    else
+        render "new"
     end
+  end
+
+  private
+    
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
