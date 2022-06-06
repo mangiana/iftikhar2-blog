4948d930a3e3252be6706f7ffcaf1392035fd466
iftikhar <iftikharmangiana.official@gmail.com>
2022-06-06 21:13:30 +0500

    Restrict Actions at Controller level for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,14 @@
 class UsersController < ApplicationController
+  before_action :find_user, only:[:show, :edit, :update, :destroy]
+  before_action :require_user, only:[:edit, :update]
+  before_action :require_same_user, only:[:edit, :update, :destroy]
+
 
   def index
     @users = User.paginate(page: params[:page], per_page: 4)
   end
 
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 4)
   end
   
@@ -14,7 +17,6 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def create
@@ -31,7 +33,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your Account Successfully Updated"
       redirect_to @user
@@ -44,6 +45,17 @@ class UsersController < ApplicationController
     
     def user_params
         params.require(:user).permit(:username, :email, :password)
+    end
+
+    def find_user
+      @user = User.find(params[:id])
+    end
+
+    def require_same_user
+      if current_user != @user
+        flash[:alert] = "You can only Update, or Delete your own Account"
+        redirect_to @user
+      end
     end
 
 end
